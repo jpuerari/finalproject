56cb650cddba87b946ed613bce7abcc9c9a6fcc5
Joseph D. Puerari <joep@Joes-MacBook-Pro.local>
2020-04-27 20:47:03 -0400

    added user password bcrypt

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./pages/Login"
+import Home from "./pages/Home"
 import "./App.css";
 
 class App extends Component {
@@ -16,7 +17,7 @@ class App extends Component {
       <Router>
       <div>
         {/* <NavTabs /> */}
-        {/* <Route exact path="/" component={Home} /> */}
+        <Route exact path="/home" component={Home} />
         <Route exact path="/" component={Login} />
       </div>
       </Router>
