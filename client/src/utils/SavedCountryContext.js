0fd0637ddce989a26bbc3769cb3fe6d4dd50a1a2
lili gugushvili <lilikog@yahoo.com>
2020-04-29 17:49:52 -0400

    new files

---
+++ client/src/utils/SavedCountryContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// model our saved country state for context
+// running this gives us our Provider & Consumer
+const SavedCountryContext = React.createContext([]);
+
+export default SavedCountryContext;
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -35,3 +35,4 @@ async function seedDb(){
     console.log(error)
   }
 }
+
